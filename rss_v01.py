8d53663c2a72d5a5f9c57bc4d62f93f3023351ae
exprms <72883406+exprms@users.noreply.github.com>
2020-12-27 09:38:27 +0100

    Update rss_v01.py

--- rss_v01.py
+++ rss_v01.py
@@ -4,9 +4,16 @@
 
 import feedparser as fp
 
-newsfeed = fp.parse('https://www.cnbc.com/id/15839135/device/rss/rss.html')
+# as an example RSS feeds from www.cnbc.com were used:
+cnbc_rss_feeds = dict([('earnings', 'https://www.cnbc.com/id/15839135/device/rss/rss.html'),
+                       ('finance', 'https://www.cnbc.com/id/10000664/device/rss/rss.html'),
+                       ('politics', 'https://www.cnbc.com/id/10000113/device/rss/rss.html')])
 
-# get all titles and sumarys:
-for i in range(len(newsfeed)):
-    print('TITLE: \n' + newsfeed.entries[i].title)
-    print('SUMMARY: \n' + newsfeed.entries[i].summary)
+for feed in cnbc_rss_feeds.values():
+    tempfeed = fp.parse(feed)
+
+    # get all titles and summaries:
+    print('FROM FEED: ' + feed)
+    for i in range(len(tempfeed)):
+        print('*********************************************** \n' + tempfeed.entries[i].title)
+        print('SUMMARY: \n' + tempfeed.entries[i].summary)
